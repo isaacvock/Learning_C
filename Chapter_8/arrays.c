853ee45de5c4c6abbde657db8dd7cf505fa03ef8
isaacvock <isaac.vock@gmail.com>
2024-08-08 22:57:55 -0400

    Finished chapter on Arrays

--- Chapter_8/arrays.c
+++ Chapter_8/arrays.c
@@ -1,8 +1,77 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
 #define N 10
+#define NUM_SUITS 4
+#define NUM_RANKS 13
 
 int main(void){
+
+    /* Dealing a hand of cards */
+    int handsize;
+    int rand_rank;
+    int rand_suit;
+
+    srand(time(NULL));
+
+
+    const char rank_code[] = {'2', '3', '4', '5', '6', 
+                              '7', '8', '9', 't', 'j',
+                              'q', 'k', 'a'};
+    const char suit_code[] = {'c', 'd', 'h', 's'};
+
+    bool in_hand[NUM_SUITS][NUM_RANKS] = {false};
+
+
+    printf("Enter number of cards in hand: ");
+    scanf("%d", &handsize);
+
+
+    printf("Your hand: ");
+    for(int h = 0; h < handsize; h++){
+
+        int new_card = 0;
+        while(new_card == 0){
+
+            rand_rank = rand() % NUM_RANKS;
+            rand_suit = rand() % NUM_SUITS;
+
+            if(!(in_hand[rand_suit][rand_rank])){
+                in_hand[rand_suit][rand_rank] = true;
+                new_card = 1;
+                printf("%c%c ", rank_code[rand_rank], suit_code[rand_suit]);
+            }
+
+
+        }
+
+    }
+    printf("\n");
+
+
+
+    /* Multi-dimensional arrays */
+
+    int m[2][4] = {{1, 1, 1, 2},
+                   {3, 3, 4, 4}};
+
+    double ident[N][N];
+
+    for(int x = 0; x < N; x++){
+
+        for(int y = 0; y < N; y++){
+
+            if(y == x){
+                ident[x][y] = 1;
+            }else{
+                ident[x][y] = 0;
+            }
+
+        }
+
+    }
 
     /* Check number of repeated digits */
     int number;
