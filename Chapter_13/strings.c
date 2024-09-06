41cafe8fbca5d6eff280aacba750e8a626923f80
isaacvock <isaac.vock@gmail.com>
2024-09-06 08:45:10 -0400

    Learning about strings

---
+++ Chapter_13/strings.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+#include <time.h>
+
+int main(void){
+
+    /* Some string literal stuff */
+    printf("When you come to a fork in the road, take it. \
+    --Yogi Berra\n");
+
+    char *p;
+    p = "abc";
+
+    printf("%s\n", p);
+}
